c4e28018e066979727398f657ad815931411a256
Xavi <xavierllubes11@gmail.com>
2019-04-14 19:41:26 +0200

    Added login and signup pages

--- HSDT/urls.py
+++ HSDT/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
+from django.urls import path, include
+
+
 from . import views
 
 app_name = "HSDT"
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.SignUp.as_view(), name='signup'),
     url(r'cards/', views.cards, name='cards'),
     url(r'decks/', views.decks, name='decks'),
 
--- HSDT/views.py
+++ HSDT/views.py
@@ -1,9 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views import generic
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/login.html'
 
 
 def cards(request):
