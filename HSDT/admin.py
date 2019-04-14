d1e3a0d0519e692d2b2e6d27376ae387c7a3ecee
joelk <joelkinsell@gmail.com>
2019-04-14 20:50:13 +0200

    Created Models

--- HSDT/admin.py
+++ HSDT/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from HSDT.models import *
 
 # Register your models here.
+admin.site.register(Cards)
+admin.site.register(Deck)
+admin.site.register(CardsInDeck)
--- HSDT/models.py
+++ HSDT/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+CLASSES = ((0, "Druid"), (1, "Hunter"), (2, "Mage"), (3, "Paladin"), (4, "Priest"), (5, "Rogue"), (6, "Shaman"), (7, "Warlock"), (8, "Warrior"), (9, "Dream"), (10, "Neutral"),)
+
+SETS = ((0, "Basic"), (1, "Classic"), (2, "Promo"), (3, "Hall of Fame"), (4, "Naxxramas"), (5, "Goblins vs Gnomes"), (6, "Blackrock Mountain"), (7, "The Grand Tournament"),
+        (8, "The League of Explorers"), (9, "Whispers of the Old Gods"), (10, "One Night in Karazhan"), (11, "Mean Streets of Gadgetzan"), (12, "Journey to Un'Goro"),
+        (13, "Knights of the Frozen Throne"), (14, "Kobolds & Catacombs"), (15, "The Witchwood"), (16, "The Boomsday Project"), (17, "Rastakhan's Rumble"), (18, "Rise of Shadows"),
+        (19,"Tavern Brawl"), (20, "Taverns of Time"), (21, "Hero Skins"), (22, "Missions"), (23, "Credits"), (24, "System"), (25, "Debug"),)
+
+TYPES = ((0, "Hero"), (1, "Minion"), (2, "Spell"), (3, "Enchantment"), (4, "Weapon"), (5, "Hero Power"),)
+
+QUALITIES = ((0, "Free"), (1, "Common"), (2, "Rare"), (3, "Epic"), (4, "Legendary"),)
+
+RACES = ((0, "Demon"), (1, "Dragon"), (2, "Elemental"), (3, "Mech"), (4, "Murloc"), (5, "Beast"), (6, "Pirate"), (7, "Totem"),)
+
+
+class Cards(models.Model):
+    cardID = models.CharField("Card ID", max_length=8, primary_key=True)
+    DeckStringID = models.CharField("Deck String ID", max_length=10)
+    name = models.CharField("Name", max_length=30)
+    cardSet = models.CharField("Card Set", max_length=100, choices=SETS)
+    type = models.CharField("Type", max_length=30, choices=TYPES)
+    rarity = models.CharField("Rarity", max_length=30, choices=QUALITIES)
+    cost = models.IntegerField("Cost")
+    attack = models.IntegerField("Attack")
+    health = models.IntegerField("Health")
+    text = models.TextField("Text", default="")
+    race = models.CharField("Race", max_length=30, choices=RACES)
+    playerClass = models.CharField("Player Class", max_length=30, choices=CLASSES)
+    img = models.URLField("Image")
+
+    def __str__(self):
+        return self.name
+
+
+class Deck(models.Model):
+    name = models.CharField("Name", max_length=200)
+    playerClass = models.CharField("Player Class", max_length=30, choices=CLASSES)
+    deckString = models.CharField("Deck String", max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class CardsInDeck(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    card = models.ForeignKey(Cards, on_delete=models.CASCADE)
+    numOfCards = models.IntegerField("Number of Cards")
+
+    def __str__(self):
+        return self.name
