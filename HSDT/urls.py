274ff068a5e2412367220de5caf2fc4dfc073d10
viloc98 <viloc98@gmail.com>
2019-04-14 19:35:24 +0200

    Pagina de cartes i baralles

--- HSDT/urls.py
+++ HSDT/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import url
 from . import views
+
+app_name = "HSDT"
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'cards/', views.cards, name='cards'),
+    url(r'decks/', views.decks, name='decks'),
+
 ]
--- HSDT/views.py
+++ HSDT/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+
+def cards(request):
+    return render(request, 'cards.html')
+
+
+def decks(request):
+    return render(request, 'decks.html')
