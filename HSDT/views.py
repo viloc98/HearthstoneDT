3b9142f645f97c540f13b9416e33a577f1c6a5ef
Xavi <xavierllubes11@gmail.com>
2019-05-09 20:25:28 +0200

    Added api cards

--- HSDT/views.py
+++ HSDT/views.py
@@ -1,6 +1,6 @@
+import requests
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.decorators import login_required
@@ -18,7 +18,12 @@ class SignUp(generic.CreateView):
 
 @login_required(login_url='/HSDT/accounts/login')
 def cards(request):
-    return render(request, 'cards.html')
+    url = 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards'
+    headers = {"X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com", "X-RapidAPI-Key": "39bec267c8mshd1a526c1f59a705p1a5cb3jsndf3b6d749365"}
+    r = requests.get(url, headers=headers)
+    json = r.json()
+    print(json['Basic'])
+    return render(request, 'cards.html', context=json)
 
 
 @login_required(login_url='/HSDT/accounts/login')
